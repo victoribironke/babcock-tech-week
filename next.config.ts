f8e57cb5e80cc8f7558887d5e41ef0ed10ec896a
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-20 11:58:49 +0100

    feat: add /calendar redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/calendar",
+        destination: "https://lu.ma/btw26",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
