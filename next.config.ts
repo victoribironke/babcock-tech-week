d52620f10604b3b54cc889ca08008852d890b7c8
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-23 09:13:24 +0100

    feat: add hackathon redirects

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,18 @@ const nextConfig: NextConfig = {
         destination: "https://lu.ma/btw26",
         permanent: false,
       },
+      {
+        source: "/hackathon",
+        destination:
+          "https://docs.google.com/forms/d/1eG0InqhmCQQBCGAGVDXqhGX5ONaNkERglKmrhiSVqjk/edit?ts=69c06368",
+        permanent: false,
+      },
+      {
+        source: "/hackathon/terms",
+        destination:
+          "https://docs.google.com/document/d/1hMa_9QZhSNA7FV-pRj1HlI_ntcLYnQxm/edit",
+        permanent: false,
+      },
     ];
   },
 };
