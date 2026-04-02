a24d9645fbdf87c2d14d1784638ceb4c3009b9f2
Kachi Obata <kachiobata@gmail.com>
2026-04-02 23:20:00 +0100

    Changed the nav bar on this page to render in as solid from the top of the page to avoid disappearing against white background

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function TeamPage() {
   return (
     <>
-      <Header />
+      <Header solid />
       <main>
         <TeamOrganogram />
       </main>
